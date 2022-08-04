76edf447213b3d23b0c5710b55472dbd7434d895
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-08-04 13:37:34 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function introduction( name){
+    var name=Aki
+    console.log(`Hi,my name is ${name}.`);
+}
+function introductionWithLanguage( name,language){
+   var name=Aki
+    console.log(`Hi,my name is "${name}" and  I am learning to program in "${language}".`);
+}
+function introductionWithLanguageOptional( name,language="Javascript"){
+   var name=Gracie
+    console.log(`Hi,my name is "${name} "and  I am learning to program in "${language}".`);
+}
+
+function introductionWithLanguageOptional( name,language="Python"){
+    var  name=Gracie
+    console.log(`Hi,my name is "${name} "and  I am learning to program in "${language}".`);
+}
